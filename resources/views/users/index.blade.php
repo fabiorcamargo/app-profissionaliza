2f82981d8806f932295aa40f4eec85c70ee9f3e6
Fabio Camargo <fabiorcamargo@gmail.com>
2022-11-30 17:26:35 -0300

    First Commit

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,12 @@
+<h1>Listagem dos usuários</h1>
+
+<ul>
+    @foreach ($users as $user)
+    <li>
+        {{ $user->name }} - 
+        {{ $user->email }}
+        <a href="{{ route('users.show', $user->id) }}">Detalhes</a>
+    </li>
+    @endforeach
+</ul>
+
